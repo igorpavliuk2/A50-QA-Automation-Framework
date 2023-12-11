c46c53a28ba8ccd11bc512bc344047a3cd57765e
igorpavliuk2 <igor.pavliuk@testpro.io>
2023-12-11 14:56:46 -0500

    HW20-2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.v105.indexeddb.model.Key;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Parameters;
 
-import java.security.Key;
 import java.time.Duration;
 
 
@@ -38,10 +38,14 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.get(baseUrl);
     }
+
+
+
     @AfterMethod
     public void closeBrowser() {
         getDriver().quit();
     }
+
 
     public WebDriver getDriver() {
         return driver;

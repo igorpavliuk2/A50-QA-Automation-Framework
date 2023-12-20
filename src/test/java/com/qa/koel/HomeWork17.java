eeef162bbb77bdd607b154ff5e1bfaf086df2239
igorpavliuk2 <igor.pavliuk@testpro.io>
2023-12-20 00:29:42 -0500

    HW24 GRID

--- src/test/java/com/qa/koel/HomeWork17.java
+++ src/test/java/com/qa/koel/HomeWork17.java
@@ -26,4 +26,21 @@ public void addSongToPlaylist() {
             WebElement successShow = getDriver().findElement(By.cssSelector("div[class='success show']"));
             Assert.assertTrue(successShow.isDisplayed());
     }
+    @Test (testName = "Alternative Add song to playlist ")
+    public void addSongToPlaylistAlternative() {
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.loginKoel();
+        loginPage.clickSubmit();
+        allSongPage();
+        WebElement songRandom2 = getDriver().findElement(By.xpath("//td[contains(text(),'Episode 2')]"));
+        songRandom2.click();
+        WebElement addToPlaylist2 = getDriver().findElement(By.cssSelector("button[class='btn-add-to']"));
+        addToPlaylist2.click();
+        WebElement favorites2 = getDriver().findElement(By.xpath("//li[contains(text(),'123')]"));
+        favorites2.click();
+        WebDriverWait wait = new WebDriverWait(getDriver(),Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='success show']")));
+        WebElement successShow2 = getDriver().findElement(By.cssSelector("div[class='success show']"));
+        Assert.assertTrue(successShow2.isDisplayed());
+    }
 }

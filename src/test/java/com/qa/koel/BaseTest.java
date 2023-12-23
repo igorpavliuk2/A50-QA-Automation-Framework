1fef327439abd26c8e71db1127381fec13a4e317
igorpavliuk2 <igor.pavliuk@testpro.io>
2023-12-22 20:51:44 -0500

    HW25 LAMBDATEST

--- src/test/java/com/qa/koel/BaseTest.java
+++ src/test/java/com/qa/koel/BaseTest.java
@@ -21,7 +21,7 @@ import org.testng.annotations.Parameters;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.time.Duration;
-
+import java.util.HashMap;
 
 
 public abstract class BaseTest {
@@ -29,58 +29,63 @@ public abstract class BaseTest {
 
     protected String url = "https://qa.koel.app/";
 
+    private static final ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
+
 
     @BeforeMethod
     @Parameters({"baseUrl"})
     public void setupDriver(String baseUrl) throws MalformedURLException {
 
-        driver = pickDriver(System.getProperty("browserName")); //driver = ChromeDriver instance
+        threadDriver.set(pickDriver(System.getProperty("browser"))); //driver = ChromeDriver instance
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));// one time
-        driver.manage().window().fullscreen();
-        driver.get(url); //open page
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));// one time
+        getDriver().manage().window().fullscreen();
+        getDriver().get(url); //open page
     }
 
     public WebDriver getDriver() {
-        return driver;
+        return threadDriver.get();
     }
     public void allSongPage () {
         WebElement allSongs = getDriver().findElement(By.cssSelector("a[href='#!/songs']"));
         allSongs.click();
     }
     public void dClick (String css){
-        Actions action = new Actions(driver);
+        Actions action = new Actions(getDriver());
 
-        WebElement link = driver.findElement(By.cssSelector(css));
+        WebElement link = getDriver().findElement(By.cssSelector(css));
         action.doubleClick(link).perform();
 
     }
     public void rClick (String css){
-        Actions action = new Actions(driver);
+        Actions action = new Actions(getDriver());
 
-        WebElement link = driver.findElement(By.cssSelector(css));
+        WebElement link = getDriver().findElement(By.cssSelector(css));
         action.contextClick(link).perform();
     }
     public void lClick (String xpath){
-        Actions action = new Actions(driver);
+        Actions action = new Actions(getDriver());
 
-        WebElement link = driver.findElement(By.xpath(xpath));
+        WebElement link = getDriver().findElement(By.xpath(xpath));
         action.click(link).perform();
     }
     public void deletePlaylist () {
-        WebElement deletePlaylistButton = driver.findElement(By.cssSelector("button[class='del btn-delete-playlist']"));
+        WebElement deletePlaylistButton = getDriver().findElement(By.cssSelector("button[class='del btn-delete-playlist']"));
         deletePlaylistButton.click();
-        WebElement okButton = driver.findElement(By.cssSelector("button[class='ok']"));
+        WebElement okButton = getDriver().findElement(By.cssSelector("button[class='ok']"));
         okButton.click();
 
     }
 
 
     @AfterMethod
-    public void closeBrowser() {
-        getDriver().quit();
+    //public void closeBrowser() {
+       // getDriver().quit();
+   // }
+public void tearDown() {
+        threadDriver.get().close();
+        threadDriver.remove();
     }
-
     public WebDriver pickDriver(String browserName) throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         String gridUrl = "http://10.0.0.102:4444/";
@@ -102,8 +107,10 @@ public abstract class BaseTest {
                 edgeOptions.addArguments("--remote-allow-origins=*");
                 return new EdgeDriver(edgeOptions);
             case "grid-chrome":
-                capabilities.setCapability("browserName", "chrome");
+                capabilities.setCapability("browser", "chrome");
                 return driver = new RemoteWebDriver(URI.create(gridUrl).toURL(), capabilities);
+            case "lambda-driver":
+                return getLambdaDriver();
             default:
                 WebDriverManager.safaridriver().setup();
                 return new SafariDriver();
@@ -112,5 +119,22 @@ public abstract class BaseTest {
 
 
 
+    }
+    public WebDriver getLambdaDriver () throws MalformedURLException {
+        String userName = "igor.pavliuk";
+        String authKey = "gcHjMzOxMR88LvKDJOIBwdQhgg6103wfCBDOsaMobzhDBt4M4V";
+        String hub = "@hub.lambdatest.com/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("browserVersion", "121.0");
+        HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+        ltOptions.put("username", userName);
+        ltOptions.put("accessKey", authKey);
+        ltOptions.put("video", true);
+        ltOptions.put("platformName", "Windows 10");
+        ltOptions.put("project", "Koel");
+        capabilities.setCapability("LT:Options", ltOptions);
+        return new RemoteWebDriver(URI.create("https://" + userName + ":" + authKey + hub).toURL(),capabilities);
     }
 }
